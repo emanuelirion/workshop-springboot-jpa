e6779f81627997c4a8a899fc7f0eb1783f5204af
Emanuel <2310100375>
2025-12-17 10:05:51 -0300

    Exception handling - findById

--- src/main/java/com/irion/course/services/UserService.java
+++ src/main/java/com/irion/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.irion.course.services;
 
 import com.irion.course.entities.User;
 import com.irion.course.repositories.UserRepository;
+import com.irion.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class UserService {
     }
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User obj) {
         return repository.save(obj);
---
+++ src/main/java/com/irion/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.irion.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource with id " + id + " not found");
+    }
+}
