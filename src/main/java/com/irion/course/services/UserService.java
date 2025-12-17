b42d743020fa4ac385c7ee0ef869590a5ee8ab3d
Emanuel <2310100375>
2025-12-17 10:37:02 -0300

    Exception handling - update

--- src/main/java/com/irion/course/services/UserService.java
+++ src/main/java/com/irion/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.irion.course.entities.User;
 import com.irion.course.repositories.UserRepository;
 import com.irion.course.services.exceptions.DatabaseException;
 import com.irion.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -38,9 +39,15 @@ public class UserService {
 
     }
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+
+
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw  new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
